a3a41a496fcc20273689a961feb6f3ce544122f2
Jose Rivera <jcrivera2709@eagle.fgcu.edu>
2019-10-03 12:30:10 -0400

    Added Nickname function to add a nickname and update the nickname if the user chooses to

--- AboutMe/src/main/java/com/example/aboutme/MainActivity.kt
+++ AboutMe/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,12 +1,55 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // In the code bellow, "it"  refers to the done_button, which is the view passed as the argument.
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickName(it)
+        }
+
+        //
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+            updateNickName(it)
+        }
+    }
+
+    private fun addNickName(view: View){
+        // gets the text and edit by reference
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        nicknameTextView.text = editText.text
+        editText.visibility = View.GONE
+
+        // hides the done button after clicking done
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        // removes the keyboard after entering nickname
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun updateNickName(view: View){
+        // gets button and text by reference
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val doneButton = findViewById<Button>(R.id.done_button)
+
+        // Shows the edit text, add button, and hides the text view.
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
     }
 }
